01dc3fe2a83681272752caccb9a2e0cd8fea52cf
Anna <anna.r.german1410@gmail.com>
2024-08-31 22:23:12 +0300

    feat: update js-code to switch between question and close icon while opening and closing the signs

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 window.onload = function() {
 
-	// *Показать условные знаки при клике на иконку с вопросом
+	// *Показать условные знаки при клике на иконку с вопросом (иконка меняется на "х")
 
-	let signImage = document.querySelector(".signs-icon img");
-	signImage.addEventListener("click", showSigns);
+	let questionIcon = document.querySelector(".signs-icon img:nth-of-type(1)");
+	questionIcon.addEventListener("click", showHideSigns);
 
-	function showSigns() {
-		document.querySelector(".signs-outside-image").classList.toggle("shown-signs");
+	function showHideSigns() {
+        document.querySelector(".signs-outside-image").classList.add("shown-signs"); // показать усл. знаки
+
+        questionIcon.style.display = "none";
+
+        let closeIcon = document.querySelector(".signs-icon img:nth-of-type(2)"); // иконка закрытия
+        closeIcon.style.display = "block";
+        closeIcon.style.zIndex = "9999";
+
+        closeIcon.addEventListener("click", function() {
+            questionIcon.style.display = "block";
+            questionIcon.style.zIndex = "9999";
+            closeIcon.style.zIndex = "-1";
+            document.querySelector(".signs-outside-image").classList.remove("shown-signs"); // скрыть усл. знаки
+        });
 	}
 
 	// * При клике на объект открывается информация по нему
 
 	
 
-    
+
 	
 }
