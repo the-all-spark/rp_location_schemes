27c735bf235857d06e87bf56a8ab77d24f5d3617
Anna <anna.r.german1410@gmail.com>
2024-08-31 23:04:34 +0300

    feat: add js-code to show info about the object when clicking on the polygon

--- script.js
+++ script.js
@@ -24,8 +24,38 @@ window.onload = function() {
 
 	// * При клике на объект открывается информация по нему
 
-	
+    let objects = document.querySelectorAll("svg polygon"); // объекты (полигоны)
+	let arrObjects = Array.from(objects);
 
+	let objectsInfo = document.querySelectorAll(".outside-item"); // спозиционированные блоки с информацией
+	let arrObjectsInfo = Array.from(objectsInfo);
 
-	
+	// перебор объектов
+	for(let i = 0; i < arrObjects.length; i++) {
+		arrObjects[i].addEventListener("click", function() {
+			let selectedObj = this.dataset.object; // значение атрибута объекта, по которому кликнули
+
+			// перебор блоков с информацией
+			for(let j = 0; j < arrObjectsInfo.length; j++) {
+
+				if(arrObjectsInfo[j].dataset.object === selectedObj) {
+					// выбираем блок с совпадающим с объектом атрибутом
+					let selectedObjInfo = document.querySelector('.outside-item[data-object = "' + selectedObj + '"]');
+					//console.log(selectedObjInfo);
+
+					selectedObjInfo.classList.toggle("shown-outside-item");
+					//console.log(objectsInfo);
+
+				} else {
+					// * скрытие предыдущих отображенных блоков с информацией
+					// если атрибут не совпадает, но элемент включает класс - удаляем этот класс
+
+					if(arrObjectsInfo[j].classList.contains("shown-outside-item")) {
+						arrObjectsInfo[j].classList.remove("shown-outside-item");
+					}
+				}
+			}
+		});
+	}
+
 }
