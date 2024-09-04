41532d16bf2449ff6f4a41f97ae004d60f0ea5f3
Anna <anna.r.german1410@gmail.com>
2024-09-04 14:56:39 +0300

    feat: add js to hide all opened info blocks and cancel all hightlightings on hover on list item

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ window.onload = function() {
 
 	function showHideSigns() {
         document.querySelector(".signs-outside-image").classList.add("shown-signs"); // показать усл. знаки
-
         questionIcon.style.display = "none";
 
         let closeIcon = document.querySelector(".signs-icon img:nth-of-type(2)"); // иконка закрытия
@@ -22,8 +21,18 @@ window.onload = function() {
         });
 	}
 
+	// * ОБЪЕКТЫ, ПОЛИГОНЫ, ИНФОРМАЦИЯ
+
+	let objects = document.querySelectorAll("svg polygon"); // объекты (полигоны)
+	let arrObjects = Array.from(objects);
+
+	let objectsInfo = document.querySelectorAll(".outside-item"); // спозиционированные блоки с информацией
+	let arrObjectsInfo = Array.from(objectsInfo);
+
+    let infoIcons = document.querySelectorAll(".info-icon"); // иконки i над блоком
+	let arrInfoIcons =  Array.from(infoIcons);
+
 	// * ---- При переходе (клике) по ссылке из списка локаций подсвечивается нужный объект 
-	// TODO - при клике на пункт должны закрываться все блоки в информацией по всем объектам и все выделения объектов!
 
 	let locationListItems = document.querySelectorAll(".location-list a");
 	let arrLocationListItems = Array.from(locationListItems);
@@ -31,12 +40,15 @@ window.onload = function() {
 	for(let i = 0; i < arrLocationListItems.length; i++) {
 		//console.log(arrLocationListItems[i]);
 		let idName = arrLocationListItems[i].hash.slice(1); // значение в href без символа #
-		arrLocationListItems[i].addEventListener("click", function() { switchToObject(idName) });
+		// выделение полигона при клике
+		arrLocationListItems[i].addEventListener("click", function() { switchToObject(idName) }); 
+		// скрытие всей предыдущей инф-ции при наведении
+		arrLocationListItems[i].addEventListener("mouseover", function() { hidePreviousInfo() }); 
 	}
 
 	// * функция выделяет границей полигон, на который совершен переход (по имени id)
 	function switchToObject(id) {
-		console.log(id);
+		//console.log(id);
 
 		let polygons = document.querySelectorAll(".outside-block polygon");
 		for(let i = 0; i < polygons.length; i++) {
@@ -52,24 +64,49 @@ window.onload = function() {
 
 	// при клике на уже выделенный через список объект
 	function showInfoSelectedObj(obj) {
-		console.log("Кликаем на уже выделенный объект!");
-		console.log(obj);
+		//console.log("Кликаем на уже выделенный объект!");
+		//console.log(obj);
 
 		if(obj.classList.contains("active-selected-polygon")) {
 			obj.classList.remove("active-selected-polygon");
 		} 
 	}
 
+	// * функция скрывает все выделенные ранее объекты и открытые блоки с информацией, если есть
+	// * при наведении на объект из списка
+	function hidePreviousInfo() {
+		//console.log("Прячем все выделения и инфу");
+		//console.log(arrObjects);
+		//console.log(arrObjectsInfo);
+		//console.log(arrInfoIcons);
+
+		// проверяем по объектам
+		for(let i = 0; i < arrObjects.length; i++) {
+			if(arrObjects[i].classList.contains("active-selected-polygon")) {
+				arrObjects[i].classList.remove("active-selected-polygon");
+			}
+			if(arrObjects[i].classList.contains("active-polygon")) {
+				arrObjects[i].classList.remove("active-polygon");
+			}
+		}
+
+		// проверяем по инфо
+		for(let j = 0; j < arrObjectsInfo.length; j++) {
+			if(arrObjectsInfo[j].classList.contains("shown-outside-item")) {
+				arrObjectsInfo[j].classList.remove("shown-outside-item");
+			}
+		}
+
+		// проверяем по иконкам i
+		for(let k = 0; k < arrInfoIcons.length; k++) {
+			if(arrInfoIcons[k].classList.contains("hidden-info-icon")) {
+				arrInfoIcons[k].classList.remove("hidden-info-icon");
+			}
+		}
+
+	}
+
 	// * ----- При клике на объект открывается информация по нему, прячется иконка i
-
-    let objects = document.querySelectorAll("svg polygon"); // объекты (полигоны)
-	let arrObjects = Array.from(objects);
-
-	let objectsInfo = document.querySelectorAll(".outside-item"); // спозиционированные блоки с информацией
-	let arrObjectsInfo = Array.from(objectsInfo);
-
-    let infoIcons = document.querySelectorAll(".info-icon"); // иконки i над блоком
-	let arrInfoIcons =  Array.from(infoIcons);
 
 	// перебор объектов, вызов функции
 	for(let i = 0; i < arrObjects.length; i++) {
@@ -78,7 +115,7 @@ window.onload = function() {
 
 	// * функция показа/скрытия иконки "i" выбранного полигона, показа/скрытия информации о нем
 	function showHideInfo() {
-		console.log("Прячем / показываем инфу");
+		//console.log("Прячем / показываем инфу");
 		//console.log(e.target);
 		//console.log(this);
 		//console.log(obj); 
@@ -88,11 +125,11 @@ window.onload = function() {
 		highlightPolygon(this); //  вызов функции выделения полигона 
 		showHideIcon(); // вызов функции скрытия и показа иконки i
 
+		// показ / скрытие информации
 		// перебор блоков с информацией
 		for(let j = 0; j < arrObjectsInfo.length; j++) {
 
 			if(arrObjectsInfo[j].dataset.object === selectedObj) {
-
 				// выбираем блок с совпадающим атрибутом (с объектом)
 				let selectedObjInfo = document.querySelector('.outside-item[data-object = "' + selectedObj + '"]');
 				let infoIcon = document.querySelector('.' + selectedObj + '-info-icon'); // иконка i выбранного объекта
@@ -120,9 +157,9 @@ window.onload = function() {
 
 	}
 
-	// * функция выделения выбранного полигона (перебор элементов массива объектов)
+	// * функция выделения _выбранного_ полигона (перебор элементов массива объектов)
 	function highlightPolygon(obj) {
-		console.log(obj);
+		//console.log(obj);
 
 		for(let elem of arrObjects) {
 			//console.log(elem);
